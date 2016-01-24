7f03c1f856bfee1d8c7598f95f344fa2c3f72591
Stefan Gabos <contact@stefangabos.ro>
2016-01-24 18:25:44 +0200

    Updated version number to 1.2.4

--- public/javascript/zebra_accordion.src.js
+++ public/javascript/zebra_accordion.src.js
@@ -24,7 +24,7 @@
  *  For more resources visit {@link http://stefangabos.ro/}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    1.2.3 (last revision: January 24, 2016)
+ *  @version    1.2.4 (last revision: January 24, 2016)
  *  @copyright  (c) 2011 - 2016 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Accordion
