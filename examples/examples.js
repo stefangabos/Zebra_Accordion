67c8f691f7cfa1ef1e39c8ca0dffde1f07335003
Stefan Gabos <contact@stefangabos.ro>
2017-05-28 00:02:13 +0300

    Updated examples

--- examples/examples.js
+++ examples/examples.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     new $.Zebra_Accordion('#Zebra_Accordion1');
 
     new $.Zebra_Accordion('#Zebra_Accordion2', {
-        'collapsible':  true
+        collapsible: true
     });
 
 });
